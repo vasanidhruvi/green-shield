b169df95316b392efc9957eaa199ffaa70c4de4d
Dhruvi Vasani <er.dhruvivasani@gmail.com>
2025-08-05 13:28:07 +0000

    can you replace this logo into something unique and creative one

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,10 +10,11 @@ export function Logo() {
         stroke="currentColor"
         strokeWidth="1.5"
       >
-        <path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20z" />
-        <path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10z" />
-        <path d="M12 12c-3.333 3.333-5 5-5 5a5 5 0 0 0 10 0s-1.667-1.667-5-5z" />
-        <path d="M7 17c3.333-3.333 5-5 5-5" />
+        <path d="M12 2a10 10 0 1 0 10 10" />
+        <path d="m12 18-6-6" />
+        <path d="m12 12 6-6" />
+        <path d="M6 12h12" />
+        <path d="M12 2v20" />
       </svg>
       <span className="text-lg font-bold">Carbon Nexus</span>
     </div>
